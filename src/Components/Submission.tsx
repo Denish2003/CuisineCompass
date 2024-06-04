67902cc59bbe96660eb489b278f97d9e3e077dc7
Denish Patel <dp9798@princeton.edu>
2024-06-04 15:57:10 -0400

    Style the submission form

--- src/Components/Submission.tsx
+++ src/Components/Submission.tsx
@@ -4,29 +4,31 @@ import './Submission.css';
 function Submission() {
   return (
     <div className="submission">
-        <h1>Recipe Submission Form</h1>
+        <h1 className="heading-submission">Recipe Submission Form</h1>
         <Form>
             <Form.Group className="mb-3" controlId="formBasicText">
-                <Form.Label>Recipe Name</Form.Label>
-                <Form.Control type="input" placeholder="Enter recipe name" />
+                <Form.Label className="form-label">Recipe Name</Form.Label>
+                <Form.Control className="form-input" type="input" placeholder="Enter recipe name" />
             </Form.Group>
 
             <label htmlFor="formFile" className="form-label">Recipe Image</label>
-            <input className="form-control form-control-md custom-input" id="formFileLg" type="file" />
+            <input className="form-control form-control-md custom-input form-input" id="formFileLg" type="file" />
             
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Recipe Ingredients</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+                <Form.Label className="form-label">Recipe Ingredients</Form.Label>
+                <Form.Control className="form-input" as="textarea" rows={3} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Recipe Instructions</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+                <Form.Label className="form-label">Recipe Instructions</Form.Label>
+                <Form.Control className="form-input" as="textarea" rows={3} />
             </Form.Group>
 
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
+            <div className="d-flex justify-content-center">
+                <Button className="form-button" variant="primary" type="submit">
+                    Submit
+                </Button>
+            </div>
             
         </Form>
     </div>
